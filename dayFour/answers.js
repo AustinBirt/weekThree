c39be273308316d988e03e96733e061fd1e4a6a2
Austin Birt <masao@Austins-MBP.hsd1.mi.comcast.net>
2023-05-28 11:56:22 -0400

    Get Even Answered

--- dayFour/answers.js
+++ dayFour/answers.js
@@ -1,16 +1,20 @@
-//Easy Going Question
-//Write a for loop that will log the numbers 1 through 20.
+// //Easy Going Question
+// //Write a for loop that will log the numbers 1 through 20.
 
-for(let i=0; i<=20; i++){
-  console.log(i)
-}
+// for(let i=0; i<=20; i++){
+//   console.log(i)
+// }
 
 
 
 //Get Even Question
 //Write a for loop that will log only the even numbers in 0 through 200.
 
-
+for (let i=1; i<=200; i++){
+  if(i%2===0){
+    console.log(i);
+  }
+}
 
 
 
