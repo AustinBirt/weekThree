b846a29e6940643f01b133d7d48832cfc4d7598f
Austin Birt <masao@Austins-MBP.hsd1.mi.comcast.net>
2023-05-28 12:24:46 -0400

    Wild Wild Life Answered

--- dayFour/answers.js
+++ dayFour/answers.js
@@ -18,30 +18,55 @@
 
 
 
-//Fizz Buzz Question
-//This is a classic problem that you should get really comfortable solving. If you've solved it before, try to make it more elegant and shorter.
+// //Fizz Buzz Question
+// //This is a classic problem that you should get really comfortable solving. If you've solved it before, try to make it more elegant and shorter.
 
-for (let i = 1; i<=100; i++){
-  if (i%3===0){
-    if(i%3===0 && i%5===0){
-      console.log('FizzBuzz');
-    }
-    else if (i%3===0){
-      console.log('Fizz');
-    }
-    else if (i%5===0){
-      console.log('Buzz');
-    }
-  }
-  else {
-      console.log(i);
-  }
-}
+// for (let i = 1; i<=100; i++){
+//   if (i%3===0){
+//     if(i%3===0 && i%5===0){
+//       console.log('FizzBuzz');
+//     }
+//     else if (i%3===0){
+//       console.log('Fizz');
+//     }
+//     else if (i%5===0){
+//       console.log('Buzz');
+//     }
+//   }
+//   else {
+//       console.log(i);
+//   }
+// }
 
 
 
 //Wild Life Question
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
 
+//1.Plantee just had her birthday; change Plantee's array to reflect her being a year older.
+plantee.splice(2,1, "4999")
+console.log(plantee);
+//2.Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
+// wolfy[3]="Gotham City"
+// wolfy[wolfy.lastIndexOf("Yukon Territory")]="GothamCity"
+wolfy.splice(wolfy.indexOf('Yukon Territory'), 1, "Gotham CiTY")
+console.log(wolfy);
+//3.Give D'Art a second hometown by adding "Hawkins"
+let dartUpdated=dart.concat();
+dartUpdated.push('Hawkins');
+console.log(dartUpdated);
+//4.Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the wolfy array and replace it with "Gameboy".
+// wolfy.shift();
+// wolfy.unshift('GameBoy')
+wolfy.shift();
+wolfy.splice(0, 0, 'GameBoyYYY')
+console.log(wolfy);
+
+//THIS IS ME EPERIMENTING, I UNDERSTAND THE MOST EFFICENT WAY TO DO THESE
 
 
 
