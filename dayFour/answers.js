92b56996a088c37c18ad312279c49bcf1bde5d6d
Austin Birt <masao@Austins-MBP.hsd1.mi.comcast.net>
2023-05-28 11:50:40 -0400

    Easy Going Answered

--- dayFour/answers.js
+++ dayFour/answers.js
@@ -1,18 +1,76 @@
-const sumMe = {
-    hello: 'there',
-    you: 8,
-    are: 7,
-    almost: '10',
-    done: '!'
-  };
-  let total = 0;
-  // ADD CODE HERE
-  
-  
-  for (let key in sumMe){
-    if(typeof(sumMe[key])==='number'){
-      total+=sumMe[key]
-    }
-  }
+//Easy Going Question
+//Write a for loop that will log the numbers 1 through 20.
 
-  console.log(total);
+for(let i=0; i<=20; i++){
+  console.log(i)
+}
+
+
+
+//Get Even Question
+//Write a for loop that will log only the even numbers in 0 through 200.
+
+
+
+
+
+//Fizz Buzz Question
+//This is a classic problem that you should get really comfortable solving. If you've solved it before, try to make it more elegant and shorter.
+
+
+
+
+
+//Wild Life Question
+
+
+
+
+
+//Yell at the Ninja Turtles Question
+
+
+
+
+//Methods Revisited Question
+
+
+
+
+//Where is Waldo Question
+
+
+
+
+
+//Excited Kitten Question
+
+
+
+
+//Find the Median Question
+
+
+
+
+
+//Hungry for more? Questions
+//Alien Atire Question
+
+
+
+
+
+//Dress Us Up Question
+
+
+
+
+
+//Dirty Laundry Question
+
+
+
+
+
+//inventory Question
