fc16c70d48ee81c45bebbac4122b1888398fef1e
Austin Birt <masao@Austins-MBP.hsd1.mi.comcast.net>
2023-05-28 12:54:21 -0400

    Yell at the Ninja Turtles answered

--- dayFour/answers.js
+++ dayFour/answers.js
@@ -40,40 +40,47 @@
 
 
 
-//Wild Life Question
-const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
-const sharky = ["Sharky", "shark", 20, "Left Coast"]
-const plantee = ["Plantee", "plant",  5000 , "Mordor"]
-const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
-const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+// //Wild Life Question
+// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+// const sharky = ["Sharky", "shark", 20, "Left Coast"]
+// const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+// const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+// const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
 
-//1.Plantee just had her birthday; change Plantee's array to reflect her being a year older.
-plantee.splice(2,1, "4999")
-console.log(plantee);
-//2.Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
-// wolfy[3]="Gotham City"
-// wolfy[wolfy.lastIndexOf("Yukon Territory")]="GothamCity"
-wolfy.splice(wolfy.indexOf('Yukon Territory'), 1, "Gotham CiTY")
-console.log(wolfy);
-//3.Give D'Art a second hometown by adding "Hawkins"
-let dartUpdated=dart.concat();
-dartUpdated.push('Hawkins');
-console.log(dartUpdated);
-//4.Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the wolfy array and replace it with "Gameboy".
+// //1.Plantee just had her birthday; change Plantee's array to reflect her being a year older.
+// plantee.splice(2,1, "4999")
+// console.log(plantee);
+// //2.Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
+// // wolfy[3]="Gotham City"
+// // wolfy[wolfy.lastIndexOf("Yukon Territory")]="GothamCity"
+// wolfy.splice(wolfy.indexOf('Yukon Territory'), 1, "Gotham CiTY")
+// console.log(wolfy);
+// //3.Give D'Art a second hometown by adding "Hawkins"
+// let dartUpdated=dart.concat();
+// dartUpdated.push('Hawkins');
+// console.log(dartUpdated);
+// //4.Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the wolfy array and replace it with "Gameboy".
+// // wolfy.shift();
+// // wolfy.unshift('GameBoy')
 // wolfy.shift();
-// wolfy.unshift('GameBoy')
-wolfy.shift();
-wolfy.splice(0, 0, 'GameBoyYYY')
-console.log(wolfy);
+// wolfy.splice(0, 0, 'GameBoyYYY')
+// console.log(wolfy);
 
-//THIS IS ME EPERIMENTING, I UNDERSTAND THE MOST EFFICENT WAY TO DO THESE
+// //THIS IS ME EPERIMENTING, I UNDERSTAND THE MOST EFFICENT WAY TO DO THESE
 
 
 
 
 //Yell at the Ninja Turtles Question
+let ninjaTurtles=["Donatello", "Leonardo", "Raphael", "Michaelangelo"];
+// for (let element of ninjaTurtles){
+//   console.log(ninjaTurtles.indexOf(element));
+// }
+//Printing index with for of
 
-
+for (let element of ninjaTurtles){
+  console.log(element.toUpperCase());
+}
 
 
 //Methods Revisited Question
